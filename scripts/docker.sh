06d6292df68d616466c132520e4b8c7b2f5987f1
Rootrl <danhoo@163.com>
2018-05-03 15:01:41 +0800

    docker-comose supported

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -11,3 +11,8 @@ systemctl start docker
 
 groupadd docker
 usermod -aG docker $USER
+
+
+# docker compose
+curl -L https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
